5ff1cc30ed3829e9c42491c42068719d4b9836be
Andrew Ogilvie <primalorb@gmail.com>
2021-05-29 20:40:35 -0400

    add basic handlebards render

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,4 +1,12 @@
 const router = require( 'express' ).Router();
-const withAuth = require( '../../utils/auth' );
+// const withAuth = require( '../../utils/auth' );
+
+router.get( '/', (req, res ) => {
+    res.render( 'homepage' )
+} );
+
+router.get( '/login', (req, res ) => {
+    res.render( 'login' )
+} );
 
 module.exports = router
